3ef3b68e648c7c259cf45701241aa82c63b4e48a
Naved khan <nomanali50997@gmail.com>
2024-08-19 11:07:32 +0530

    adding javascript frontend file

---
+++ public/script.js
@@ -0,0 +1,61 @@
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-items");
+const btnToggleNav = document.querySelector(".menu-btn");
+const workEls = document.querySelectorAll(".work-box");
+const workImgs = document.querySelectorAll(".work-img");
+const mainEl = document.querySelector("main");
+const yearEl = document.querySelector(".footer-text span");
+
+const toggleNav = () => {
+  nav.classList.toggle("hidden");
+
+  // Prevent screen from scrolling when menu is opened
+  document.body.classList.toggle("lock-screen");
+
+  if (nav.classList.contains("hidden")) {
+    btnToggleNav.textContent = "menu";
+  } else {
+    // When menu is opened after transition change text respectively
+    setTimeout(() => {
+      btnToggleNav.textContent = "close";
+    }, 475);
+  }
+};
+
+btnToggleNav.addEventListener("click", toggleNav);
+
+navMenu.addEventListener("click", (e) => {
+  if (e.target.localName === "a") {
+    toggleNav();
+  }
+});
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
+    toggleNav();
+  }
+});
+
+// Animating work instances on scroll
+
+workImgs.forEach((workImg) => workImg.classList.add("transform"));
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    const [entry] = entries;
+    const [textbox, picture] = Array.from(entry.target.children);
+    if (entry.isIntersecting) {
+      picture.classList.remove("transform");
+      Array.from(textbox.children).forEach(
+        (el) => (el.style.animationPlayState = "running")
+      );
+    }
+  },
+  { threshold: 0.3 }
+);
+
+workEls.forEach((workEl) => {
+  observer.observe(workEl);
+});
+
+yearEl.textContent = new Date().getFullYear();  //current year ke liye 
