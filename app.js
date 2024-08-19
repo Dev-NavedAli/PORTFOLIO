1c9e9ff1a0160842a1699192cff781656480f70d
Naved khan <nomanali50997@gmail.com>
2024-08-19 10:51:26 +0530

    serving static files

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 let port = 8080
-const path = require("path")
+const path = require("path") //for ejs
 
-app.set("view engine","ejs")
-app.set("views",path.join(__dirname,"/views"))
+app.set("view engine","ejs")    //for ejs 
+app.set("views",path.join(__dirname,"/views")) //for ejs
+
+
+app.use(express.static(path.join(__dirname,"public"))) //for serving static files
 
 app.get("/",(req,res)=>{
     res.render("index.ejs")
